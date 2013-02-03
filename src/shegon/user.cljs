a28b42369a3d45e5244f47d314c02c7c4e6cad06
Valentin Golev <me@va1en0k.net>
2013-02-03 11:04:15 +0400

    I realized theres clj->js and js-clj

--- src/shegon/user.cljs
+++ src/shegon/user.cljs
@@ -1,40 +1,24 @@
 (ns shegon.user)
 
-(let [old (.-provide js/goog)]
-  (set! js/goog.provide (fn [m] (try (old m) (catch js/Error e nil)))))
 
 (defn log [& a]
   (shegon.repl/add-output (apply str a))
-  (js/console.log (first a)))
+  (map #(js/console.log (clj->js %)) a))
+
 
 (def $ js/$)
 
-
-(defn to-js
-  "makes a javascript map from a clojure one"
-  [obj]
-  (cond (map? obj) (let [out (js-obj)]
-                        (doall (map
-                                #(aset out (to-js (first %)) (to-js (second %)))
-                                obj))
-                        out)
-        (seq? obj) (let [out (js* "[]")] (reduce #(.push out (to-js %2)) nil obj) out)
-        (or (keyword? obj) (symbol? obj)) (name obj)
-        :else           obj))
-
-
 (defn js-map [& clauses]
-  (to-js (apply hash-map clauses)))
+  (clj->js (apply hash-map clauses)))
 
 (defn load-module [module]
-  ; returns a code to execute to kill
   (let [url (.-url module)
         provides (.-provides module)]
     (log "Loading " provides)
-    ; (js/console.log (to-js provides))
 
-    (.ajax $ url (to-js {:dataType "script"}))
+    (.ajax $ url (js-map :dataType "script"))
 
+    ;; I've considered deleting modules before loading
     ; (str
     ;   (for [m provides :when (and (not= m "cljs.core")
     ;                               (not (nil? (try (js/eval m)
@@ -44,11 +28,20 @@
 
     ))
 
+;; goog.provide doesn't allow us to reload modules :-(
+(let [old (.-provide js/goog)]
+  (set! js/goog.provide
+    (fn [m]
+      (try
+        (old m)
+        (catch js/Error e nil)))))
+
+
 (defn require [& modules]
   (log "Loading asynchronously...")
   (.done
     (.ajax $ "/requires"
-      (to-js {:data {:modules modules} :type "post" :dataType "jsonp"}))
+      (js-map :data {:modules modules} :type "post" :dataType "jsonp"))
     (fn [data] (doall (map load-module data)))))
 
 
