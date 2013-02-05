98a7edea06154579aa05b662a00eb31e5f403b8e
Valentin Golev <me@va1en0k.net>
2013-02-05 16:27:54 +0400

    description

--- project.clj
+++ project.clj
@@ -1,8 +1,9 @@
 (defproject shegon "0.1.0-SNAPSHOT"
-            :description "FIXME: write this!"
+            :description "ClojureScript REPL able to require and reload your modules."
             :dependencies [[org.clojure/clojure "1.4.0"]
                            [noir "1.3.0-beta3"]
                            [org.clojure/clojurescript "0.0-1552"]
                            [clj-stacktrace "0.2.5"]
                            [jayq "2.0.0"]]
             :main shegon.server)
+
