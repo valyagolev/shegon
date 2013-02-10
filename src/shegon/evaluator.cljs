5cf555d8e01a3ba0242309d0cb940386a1f258d5
Valentin Golev <me@va1en0k.net>
2013-02-10 14:17:57 +0400

    able to run cljs

--- src/shegon/evaluator.cljs
+++ src/shegon/evaluator.cljs
@@ -1,13 +1,25 @@
 (ns shegon.evaluator
-  (:use [jayq.core :only [$ ajax attr]]))
+  (:use [jayq.core :only [$ attr ajax pipe]]))
+
 
 (defn anti-forgery-token []
   (attr ($ "#__anti-forgery-token") "value"))
 
+
 (defn compile-cljs-deferred [code]
-  (ajax {:url "/compiler"
-         :type :post
-         :data {:source               code
-                :ns                   cljs.core/*ns*
-                :__anti-forgery-token (anti-forgery-token)}
-         :dataType :jsonp}))
+  (pipe (ajax {:url "/compiler"
+               :type :post
+               :data {:source               code
+                      :ns                   cljs.core/*ns*
+                      :__anti-forgery-token (anti-forgery-token)}
+               :dataType :jsonp})
+        (fn [data]
+          {:result (.-result data)
+           :error (.-exception data)
+           :ns (.-ns data)})))
+
+
+(defn eval-cljs-deferred [code]
+  (pipe (compile-cljs-deferred code)
+    (fn [{:keys [result] :as data}]
+      (assoc data :result (js/eval result)))))
--- test/shegon/test/evaluator.cljs
+++ test/shegon/test/evaluator.cljs
@@ -3,7 +3,13 @@
   (:use-macros [shegon.test.macros :only [describe expect async-test]]))
 
 (describe "Evaluator"
-  :it "can compile javascript"
+  :it "can compile clojurescript"
     (async-test 200
       [result (eval/compile-cljs-deferred "(+ 1 2)")]
-      (expect (.-result result) "(1 + 2)")))
+      (expect (:result result) "(1 + 2)"))
+
+  :it "can eval clojurescript"
+    (async-test 200
+      [result (eval/eval-cljs-deferred "(+ 1 2)")]
+      (expect (:result result) 3))
+    )
